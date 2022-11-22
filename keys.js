8f66cde7310d62187a4f5e0d23a3d012458589cb
Samu28022601 <chunsamuel82@gmail.com>
2022-11-22 04:12:43 -0600

    Keys
    Primer proyecto de matrices java script

---
+++ keys.js
@@ -0,0 +1,30 @@
+console.clear();
+
+const dato1 ={
+    a: "1",
+    b: "2",
+    c: "3",
+};
+
+console.log(Object.keys(dato1));
+console.log(Object.values(dato1));
+
+
+const dato2 ={
+    a: "Apple",
+    b: "Microsoft",
+    c: "Google",
+};
+
+console.log(Object.keys(dato2));
+console.log(Object.values(dato2));
+
+
+const dato3 ={
+    key1: "true",
+    key2: "false",
+    key3: "undefined",
+};
+
+console.log(Object.keys(dato3));
+console.log(Object.values(dato3));
